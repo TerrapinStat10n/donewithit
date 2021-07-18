2a47d07eff65ae4a897fc0ba005e41ec74ea6369
Casey Bloom <CaseyBloom@gmail.com>
2021-07-17 21:05:34 -0400

    Grateful Dead starting page

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image, SafeAreaView, Button, Alert, Platform } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress} style={{ color: "white", fontSize: 30}}>Grateful Dead Charts</Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image source={{
+          uri: "https://sgbonline.com/wp-content/uploads/2021/03/grateful-dead-13.jpg",
+          width: 400,
+          height: 300
+        }} />
+      </TouchableHighlight>
+      <View style={{ width: 400, height: 70, backgroundColor: "blue", alignItems: "center", justifyContent: "center"}}>
+        <Text style={{color: "white"}}>
+          Log in
+        </Text>
+      </View>
+      <View style={{ width: 400, height: 70, backgroundColor: "red", alignItems: "center", justifyContent: "center"}}>
+        <Text style={{color: "white"}}>
+          Sign up
+        </Text>
+      </View>
+      <Button 
+        title="More information"
+        color="darkgreen"
+        onPress={() => Alert.alert("Check it out, bro.", "This app is under development, but it's going to be hella lit.", [
+          {text: "Rock n roll",}
+        ])} 
+      />
+      
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    // paddingTop: Platform.OS === "android" ? 20 : 0,
   },
 });
